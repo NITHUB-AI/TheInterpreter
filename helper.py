5ffedfddd28be01c6e4ac4a1149f1f5abea46f8b
Fortune Adekogbe <fortuneadekogbe@gmail.com>
2023-09-24 16:44:28 +0100

    Refactored main to work with APIs instead of models directly.

--- helper.py
+++ helper.py
@@ -1,26 +1,16 @@
-# import libraries models:
-
+from dotenv import load_dotenv
+from gradio_client import Client
+import requests
 import os
+import re
 # audio conversion
+import openai
 import librosa
 import soundfile as sf
-# s2t API
-from dotenv import load_dotenv
-import openai
-# s2t OS
-#import whisper
-# t2st
-import torch
-from seamless_communication.models.inference import Translator
-import torchaudio
 
 load_dotenv()
-# load models:
-# openai whisper
-model = ... # whisper.load_model("large")
 
-# facebook seamlessm4t
-translator = Translator("seamlessM4T_large", vocoder_name_or_card="vocoder_36langs", device=torch.device("cpu"))
+headers = {"Authorization": f"Bearer {os.getenv('HF_API_KEY')}"}
 
 def convert_audio(input_file, target_sr=16000):
     # Load the audio file
@@ -31,30 +21,36 @@ def convert_audio(input_file, target_sr=16000):
         y = librosa.resample(y, orig_sr=sr, target_sr=target_sr)
 
     # Save the audio in WAV format
-    output_file = f'{input_file[:-4]}_{target_sr}.wav'
+    file_name = re.split(r'/|\\|\.', input_file)[-2]
+    output_file = f"{file_name}_{target_sr}.wav"
     sf.write(output_file, y, 16000)
     return output_file, sr
 
 def transcribe(file_path, api=False):
     if api:
         audio_file= open(file_path, "rb")
-        result = openai.Audio.transcribe("whisper-1", audio_file, api_key=os.getenv("OPENAI_API_KEY"))
+        transcript = openai.Audio.transcribe("whisper-1", audio_file, api_key=os.getenv("OPENAI_API_KEY"))
     else:
-        result = model.transcribe(file_path, language='English', temperature=0)
-    transcript = result["text"]
+        API_URL = "https://api-inference.huggingface.co/models/openai/whisper-large-v2"
+
+        with open(file_path, "rb") as f:
+            data = f.read()
+        response = requests.post(API_URL, headers=headers, data=data)
+        transcript = response.json()['text']
+
     return transcript
 
 def seamless_t2st(transcript, translated_audio, sampling_rate):
-    translated_text, wav, sr = translator.predict(
-        transcript,
-        "t2st", 'fra', src_lang='eng'
+    client = Client("https://facebook-seamless-m4t.hf.space/")
+    result = client.predict(
+                    "T2ST (Text to Speech translation)",	# str (Option from: ['S2ST (Speech to Speech translation)', 'S2TT (Speech to Text translation)', 'T2ST (Text to Speech translation)', 'T2TT (Text to Text translation)', 'ASR (Automatic Speech Recognition)'])				
+                    "file",	# str in 'Audio source' Radio component
+                    "https://github.com/gradio-app/gradio/raw/main/test/test_files/audio_sample.wav",
+                    "https://github.com/gradio-app/gradio/raw/main/test/test_files/audio_sample.wav",
+                    f"{transcript}[END]",
+                    "English",
+                    "French",
+                    api_name="/run"
     )
-    wav = wav[0].cpu()
-    
-
-    torchaudio.save(
-        translated_audio,
-        wav,
-        sample_rate=sr,
-    )
+    translated_audio, translated_text = result
     return translated_text, translated_audio
--- interpreter.py
+++ interpreter.py
@@ -1,18 +1,24 @@
 from helper import convert_audio, transcribe, seamless_t2st
 import argparse
+import re
 
 def speech_to_speech(file_path):
     print("Start...")
     target_sr = 16000
+
     print(f"Resampling file to {target_sr}...")
     converted_file_path, sampling_rate = convert_audio(file_path, target_sr)
+
     print("Transcribing audio file...")
     en_text = transcribe(converted_file_path, api=True)
-    file_name = file_path.split('/')[-1][:-4]
+
     print("translating text and generating speech...")
+    file_name = re.split(r'/|\\|\.', file_path)[-2]
     translated_text, translated_audio = seamless_t2st(en_text, f"fr_{file_name}.wav", sampling_rate)
+
     print(f"Resampling file to {sampling_rate}...")
     translated_audio, _ = convert_audio(translated_audio, target_sr=sampling_rate)
+    
     print("End.")
     return translated_text, translated_audio
 
@@ -21,7 +27,9 @@ if __name__ == "__main__":
     parser.add_argument('audio_path', type=str, help='Path to the audio file')
     
     args = parser.parse_args()
-    
+    import time
+    a = time.time()
     translated_text, translated_audio = speech_to_speech(args.audio_path)
+    print(time.time() - a)
     print(translated_text)
     print(translated_audio)
