b7a781d6591cbf0b65b5605eeeab3ff7c76dfad1
Fortune Adekogbe <fortuneadekogbe@gmail.com>
2023-09-25 21:25:36 +0100

    updated api param.

--- interpreter.py
+++ interpreter.py
@@ -7,7 +7,7 @@ def speech_to_speech(file_path):
     print(f"Resampling file to {target_sr}...")
     converted_file_path, sampling_rate = convert_audio(file_path, target_sr)
     print("Transcribing audio file...")
-    en_text = transcribe(converted_file_path, api=True)
+    en_text = transcribe(converted_file_path)
     file_name = file_path.split('/')[-1][:-4]
     print("translating text and generating speech...")
     if MODE == "2STEP":
