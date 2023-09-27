a7aeb0040bd3545e1e60354272c0b4ff659951ee
John Afolayan <jnafolayan@gmail.com>
2023-09-28 00:39:50 +0100

    refactor ffmpeg usage and tests

--- utils/files.py
+++ utils/files.py
@@ -1,8 +1,16 @@
 import time
 import uuid
 
-def generate_filename(prefix='', extension=''):
+# prefer os.mktemp() where possible?
+def generate_filename(prefix='tmp_', extension=''):
+    if not extension:
+        raise ValueError("extension must be specified")
+
     random_string = str(uuid.uuid4().hex[:6])
     current_time = int(time.time())
     filename = f'{prefix}{current_time}_{random_string}{extension}'
     return filename
+
+def get_audio_bytes(file: str):
+    with open(file, 'rb') as audio:
+        return audio.read()
