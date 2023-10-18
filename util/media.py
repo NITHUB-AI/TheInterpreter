d167f9284e40dd98e9e3b334588924b049ab4703
Fortune Adekogbe <fortuneadekogbe@gmail.com>
2023-10-18 14:47:34 +0100

    commented command prints

--- util/media.py
+++ util/media.py
@@ -17,7 +17,7 @@ def extract_audio_and_video_parts(video_file: str, output_dir: str) -> tuple: #[
            .output(out_audio)
            .build())
     audio_retcode = subprocess.call(cmd, shell=True)
-    print(cmd)
+    # print(cmd)
 
     # extract video
     cmd = (FfmpegCommand()
@@ -26,7 +26,7 @@ def extract_audio_and_video_parts(video_file: str, output_dir: str) -> tuple: #[
            .output(out_video)
            .build())
     video_retcode = subprocess.call(cmd, shell=True)
-    print(cmd)
+    # print(cmd)
 
     return None if audio_retcode != 0 else out_audio, None if video_retcode != 0 else out_video
 
@@ -40,7 +40,7 @@ def stitch_audio_and_video(audio_file: str, video_file: str, output_file: str) -
            .arg('c:a', 'aac')
            .output(output_file)
            .build())
-    print(cmd)
+    # print(cmd)
     retcode = subprocess.call(cmd, shell=True)
     return True if retcode == 0 else False
 
